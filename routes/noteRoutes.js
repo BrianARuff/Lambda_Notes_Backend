fb571ab2e6e601b571d95979a8bd46d6619dce9d
BrianARuff <thelonewolf198927@gmail.com>
2018-10-29 15:31:20 -0400

    created PUT endpoint for notes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -27,7 +27,45 @@ router.post("/", (req, res) => {
   const note = { tags, title, textBody, __v };
   db.insert(note)
     .into("notes")
-    .then(count => res.status(200).json({ message: "Posted Noted", count }))
+    .then(count => res.status(200).json({ message: "Posted Note", count }))
+    .catch(err =>
+      res.status(500).json({ message: "Internal Server Error", err })
+    );
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db("notes")
+    .where({ id })
+    .first()
+    .del()
+    .then(bool => {
+      if (bool) {
+        res.status(200).json({ message: "Deleted Note", bool });
+      } else {
+        res.status(401).json({ message: "Note was not deleted", bool });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ message: "Internal Server Error", err })
+    );
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { tags, title, textBody } = req.body;
+  const note = { tags, title, textBody };
+  db("notes")
+    .where({ id })
+    .first()
+    .update(note)
+    .then(count => {
+      if (!count || !id) {
+        res.status(401).json({ message: "Note not updated", count });
+      } else {
+        res.status(200).json({ message: "Note updated", count });
+      }
+    })
     .catch(err =>
       res.status(500).json({ message: "Internal Server Error", err })
     );
