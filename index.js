2b725ba9aad56ff025c0552948f0ef31d39a669f
BrianARuff <thelonewolf198927@gmail.com>
2018-10-30 12:31:40 -0400

    removed process.env.PORT from index.js.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const server = require("./server");
 
 // set/get port number for port to listen on
-const port = process.env.PORT || 9000;
+const port = 9000;
 
 // init listener
 server.listen(port, () => console.log(`\n*** API running port: ${port}\n`));
