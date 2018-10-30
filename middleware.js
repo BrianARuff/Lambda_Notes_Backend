f165a39a0feff91802f29515548f39bacf83d70d
BrianARuff <thelonewolf198927@gmail.com>
2018-10-30 14:29:22 -0400

    added in session config

--- middleware.js
+++ middleware.js
@@ -2,10 +2,13 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
+const session = require("express-session");
+const { sessionConfig } = require("./authentication/session");
 
 module.exports = server => {
   server.use(express.json());
   server.use(helmet());
   server.use(morgan("dev"));
   server.use(cors());
+  server.use(session(sessionConfig));
 };
