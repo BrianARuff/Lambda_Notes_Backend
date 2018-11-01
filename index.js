ca8292b82146b2ff64d41d9a6b392bbd66a95613
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 20:41:23 -0400

    removed PORT from index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-require('dotenv').config(); // loads .env configuration
+require("dotenv").config(); // loads .env configuration
 
 const server = require("./server"); // get server routes & middlware
 
 // set port number for port to listen on
 const port = process.env.PORT || 9000;
 
-// init listener
-server.listen(port, () => console.log(`\n*** API running port: ${port}, name: ${process.env.DB_NAME}, user: ${process.env.DB_USER}, password: ${process.env.DB_PASS}, db: ${process.env.DB}\n`));
+const instance = server.listen(port, () =>
+  console.log(`\n=== Server Runnning on port: ${instance.address().port} ===\n`)
+);
