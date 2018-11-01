4f88e2f5a18ea6edc45d31bd6b588501e5fbb664
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 20:32:19 -0400

    removed DB from env

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const server = require("./server"); // get server routes & middlware
 const port = process.env.PORT || 9000;
 
 // init listener
-server.listen(port, () => console.log(`\n*** API running port: ${port}\n`));
+server.listen(port, () => console.log(`\n*** API running port: ${port}, name: ${process.env.DB_NAME}, user: ${process.env.DB_USER}, password: ${process.env.DB_PASS}, db: ${process.env.DB}\n`));
